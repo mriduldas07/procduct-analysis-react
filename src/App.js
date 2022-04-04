c5329caeb1d7bb676cf18ae90be7dbd254a876be
mriduldas07 <mriduldas0325@gmail.com>
2022-04-04 13:57:23 +0600

    home section

--- src/App.js
+++ src/App.js
@@ -2,13 +2,14 @@ import logo from './logo.svg';
 import './App.css';
 import { Route, Routes } from 'react-router-dom';
 import Header from './components/Header/Header';
+import Home from './components/Home/Home';
 
 function App() {
   return (
     <div className="App">
       <Header></Header>
       <Routes>
-        
+        <Route path='/home' element={<Home></Home>}></Route>
       </Routes>
     </div>
   );
---
+++ src/components/Home/Home.js
@@ -0,0 +1,21 @@
+import React from 'react';
+import './Home.css'
+import img from '../../image.png'
+
+const Home = () => {
+    return (
+        <div className='home-container'>
+            <div className="home-text">
+                <h1 id='first-h1'>Welcome to the knowledge of Books</h1>
+                <h1 id='second-h1'>The ultimate power of Straggle</h1>
+                <p>Millions are out of work and a drought has broken the Great Plains. Farmers are fighting to keep their land and their livelihoods as the crops are failing, the water is drying up, and dust threatens to bury them all. One of the darkest periods of the Great Depression, the Dust Bowl era, has arrived with a vengeance.</p>
+                <button>Live demo</button>
+            </div>
+            <div className="home-img">
+                <img src={img} alt="" />
+            </div>
+        </div>
+    );
+};
+
+export default Home;
