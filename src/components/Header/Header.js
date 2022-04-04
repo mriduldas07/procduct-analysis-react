368ca4ea4e01eb3631359bbbabb5edc0cd3a6427
mriduldas07 <mriduldas0325@gmail.com>
2022-04-04 12:18:46 +0600

    set up

---
+++ src/components/Header/Header.js
@@ -0,0 +1,16 @@
+import React from 'react';
+import { Link } from 'react-router-dom';
+import './Header.css'
+
+const Header = () => {
+    return (
+        <div className='navbar'>
+            <Link to="/home">HOME</Link>
+            <Link to="/reviews">REVIEWS</Link>
+            <Link to="/dashboard">DASHBOARD</Link>
+            <Link to="/blogs">BLOGS</Link>
+        </div>
+    );
+};
+
+export default Header;
